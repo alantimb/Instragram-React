6f8f1e8c449366ce24d91f797591909342d0f5d7
Alan Timbó <alancorreiatimbo@gmail.com>
2022-11-19 22:59:02 -0300

    completed project components

---
+++ src/Story.js
@@ -0,0 +1,14 @@
+export default function Story() {
+  // implementar lógica dos stories mapeados do array
+
+  return (
+    <>
+      <div class="story">
+        <div class="imagem">
+          <img src="assets/img/9gag.svg" />
+        </div>
+        <div class="usuario">9gag</div>
+      </div>
+    </>
+  );
+}
---
+++ src/Usuario.js
@@ -0,0 +1,18 @@
+export default function Usuario() {
+  // implementar lógica de alteração dos dados do usuário
+
+  return (
+    <>
+      <div class="usuario">
+        <img src="assets/img/catanacomics.svg" />
+        <div class="texto">
+          <strong>catanacomics</strong>
+          <span>
+            Catana
+            <ion-icon name="pencil"></ion-icon>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
